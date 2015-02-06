ed58c87290a327e72ccfcdb5cb9f4e27b1b3e623
Abhimannu Majumder <abhimannum97@gmail.com>
2015-02-05 23:15:50 -0500

    added factorial and fibbonacci

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,3 +1,29 @@
 public class Recursion{
 
+    public String name(){
+	return "Abhimannu Majumder";
+    }
+
+    public int fact(int n){
+	if (n == 1){
+	    return 1;
+	}
+	return n * fact(n-1);
+    }
+
+    public int fib(int n){
+	if (n == 0){
+	    return 0;
+	}
+	if (n == 1){
+	    return 1;
+	}
+	return fib(n-1) + fib(n-2);
+    }
+    
+    public static void main(String[]args){
+	Recursion x = new Recursion();
+	System.out.println(x.fib(6));
+
+    }
 }
