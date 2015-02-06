be2a6af754cad33adbeeb5571db1ff5230deeacf
Abhimannu Majumder <abhimannum97@gmail.com>
2015-02-06 15:00:04 -0500

    added sqrt and IllegalArg Exceptions

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -5,6 +5,9 @@ public class Recursion{
     }
 
     public int fact(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
 	if (n == 1){
 	    return 1;
 	}
@@ -12,6 +15,9 @@ public class Recursion{
     }
 
     public int fib(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
 	if (n == 0){
 	    return 0;
 	}
@@ -21,9 +27,28 @@ public class Recursion{
 	return fib(n-1) + fib(n-2);
     }
     
+       public double sqrt(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return sqrtHelp(n, 1);
+    }
+
+    public double sqrtHelp(double n, double guess){
+	if (n == 0){
+	    return 0;
+	}else if (guess * guess == n){
+	    return guess;
+	}else{
+	    return sqrtHelp(n, (n / guess + guess) / 2);
+	}
+    }
+    
     public static void main(String[]args){
 	Recursion x = new Recursion();
 	System.out.println(x.fib(6));
 
     }
+    
+   
 }
