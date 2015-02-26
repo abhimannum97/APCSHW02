8bbf6b7f3a1247f31a5a818138ce5e498a48f908
Abhimannu Majumder <abhimannum97@gmail.com>
2015-02-26 00:10:52 -0500

    finished up NQueens

--- 03NQueens/NQueens.java
+++ 03NQueens/NQueens.java
@@ -10,13 +10,17 @@ public class NQueens{
     final static String show =  "\033[?25h";
     
     //instance variable
-    private int[][]board;
+    private char[][]board;
     private int size;
 
     
     //terminal specific character to move the cursor
     private String go(int x,int y){
 	return ("\033[" + x + ";" + y + "H");
+    }
+
+    public String name(){
+	return "majumder.abhimannu";
     }
     
     public void wait(int millis){
@@ -41,35 +45,50 @@ public class NQueens{
     }
     
     public NQueens(int size1){
-	board = new int[size1][size1];
+	board = new char[size1][size1];
 	size = size1;
 	for (int rows = 0; rows < size; rows++){
 	    for (int cols = 0; cols < size; cols++){
-		board[rows][cols] = 0;
+		board[rows][cols] = '.';
 	    }
 	}
     }
 	
-	
+    public boolean solve(){
+	int[] coordinates = new int[size];
+	for (int i = 0; i < size; i++){
+	    coordinates[i] = -1;
+	}
+	return solve(0,0,0,coordinates);
     }
-    
-    
-    public void solve(){
-	
-    }
-    
-    public void solve(int startx, int starty){
-				
-    }
-    
-    
-    
-    public boolean solve(int x,int y,int currentMoveNumber){
+					 
+    public boolean solve(int y,int x,int count,int[] coordinates){
 	System.out.println(this);
 	wait(20);
-	
-	return false;
+	if (count == size){
+	    return true;
+	}
+	if (y < 0 || y >= size|| x < 0 || x >= size){
+	    return false;
+	}
+	board[y][x] = 'Q';
+	coordinates[y] = x;
+	for (int i = 0; i < y;i++){
+	    if (x == coordinates[i] || (float)(x - coordinates[i])/(y-i) == 1 || (float)(x - coordinates[i])/(y - i) == -1){
+		board[y][x] = '.';
+		return solve(y,x+1,count,coordinates);
+	    }
+	}
+	if (solve(y + 1, 0, count + 1, coordinates)){
+	    return true;
+	}
+	board[y][x] = '.';
+	return solve(y, x+1,count,coordinates);
     }
+
+	public static void main(String[]args){
+	    NQueens x = new NQueens(10);
+	    x.solve();
+	}
     
-    
-}
+    }
