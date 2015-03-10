e89f9975327f8c33dd3ee29d0fc80da7f19fc897
Abhimannu Majumder <abhimannum97@gmail.com>
2015-03-10 06:13:50 -0400

    parition finished

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -10,53 +10,73 @@ public class QuickSelect{
 	return "[" + ans + "]";
     }
 
-    public static void partition(int[] ary, int si, int ei){
+    public static void oldpartition(int[] ary, int si, int ei){
 	System.out.println(Arrays.toString(ary));
-	/*int[] D = new int[ary.length];
+	int[] D = new int[ary.length];
 	for (int i = 0; i < ary.length; i++){
 	    if (i < si || i > ei){
 		D[i] = ary[i];
 	    }
 	}
-	*/
 	int start = si;
 	int end = ei;
 	int ri = si + (int)(Math.random() * (ei - si + 1));
 	int pivot = ary[ri];
 	System.out.println("pivot =" + pivot);
-	System.out.println(Arrays.toString(ary));
-	int holder = 0;
+	System.out.println(Arrays.toString(D));
 	for (int i = start; i <= end; i++){
-	    if (si != ei){
 	    if(ary[i] < pivot){
-		holder = ary[si];
-		ary[si] = ary[i];
-		ary[i] = holder;
+		D[si] = ary[i];
 		si++;
 	    }else if (ary[i] > pivot){
-		holder = ary[ei];
-		ary[ei] = ary[i];
-		ary[i] = holder;
+		D[ei] = ary[i];
 		ei--;
 	    }
-	    }
-	     System.out.println(Arrays.toString(ary));
+	    
+	     System.out.println(Arrays.toString(D));
 	}
-       
+	D[ri] = pivot;
+	System.out.println(Arrays.toString(D));
+    }
+
+    public static int partition(int[] ary, int si, int ei){
 	System.out.println(Arrays.toString(ary));
+	int ri = ary[si + (int)(Math.random() * (ei - si))];
+	System.out.println("pivot = " + ri);
+	int trace = si;
+	int holder;
+	while (si != ei){
+	    if (ary[trace] > ri){
+		holder = ary[ei];
+		ary[ei] = ary[trace];
+		ary[si] = holder;
+		ei--;
+	    }
+	    if (ary[trace] < ri){
+		si++;
+		trace++;
+	    }
+	    if (ary[trace] == ri){
+		trace++;
+	    }
+	    System.out.println(Arrays.toString(ary));
+	}
+	//ary[si] = ri;
+	return si;
     }
 
     public static int quickSelect(int ary[], int n){
 	return 4;
     }
+
+    
     
     public static void main(String[]args){
 	int[] A = new int[10];
 	Random rand = new Random();
 	for (int i = 0; i < A.length; i++){
 	    A[i] = rand.nextInt(100);
-	}
-	
+	}	
 	QuickSelect qs = new QuickSelect();
         
 	qs.partition(A, 0, 9);
