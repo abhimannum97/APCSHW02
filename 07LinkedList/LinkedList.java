247e406d17b739c174a44fc4f0a6d4ba15414375
Abhimannu Majumder <abhimannum97@gmail.com>
2015-03-15 22:19:18 -0400

    added remove, get, set, indexOf

--- 07LinkedList/LinkedList.java
+++ 07LinkedList/LinkedList.java
@@ -13,6 +13,26 @@ public class LinkedList{
 	return size;
     }
 
+    public int get(int index){
+	LNode current = node.getNext();
+	int i = 0;
+	while(i < index){
+	    current = node.getNext();
+	}
+	return current.getData();
+    }
+
+    public int indexOf(int val){
+	LNode current = node;
+	int ind = 0;
+	while(current.getNext() != null){
+	    if (val == current.getData()){
+		return ind;
+	    }
+	}
+	return -1;
+	}
+	
     public boolean add(int val){
 	LNode temp = node;
 	if (temp.size() == 0){
@@ -32,8 +52,33 @@ public class LinkedList{
 	for (int i = 0; i < index; i++){
 	    temp = temp.getNext();
 	}
-	temp.setNext(new LNode(cal, current.getNext()));
+	temp.setNext(new LNode(value, current.getNext()));
 	return true;
     }
+   
+    public int set(int ind, int val){
+	LNode current = node;
+	int i = 0;
+	while (i < ind){
+	    current = current.getNext();
+	}
+	int temp = current.getData();
+	current.setData(val);
+	return temp;
+    }
+
+    public int remove(int ind){
+	LNode current = node;
+	int i = 0;
+	while (i < ind - 1){
+	    current = current.getNext();
+	}
+	int x = current.getNext().getData();
+	current.setNext(current.getNext().getNext());
+	return x;
+    }
     
+    
+
 }
+
