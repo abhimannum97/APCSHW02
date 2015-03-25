7f238b9b7b88f2a4d4017dc05c904ebfd7bd8b19
Abhimannu Majumder <abhimannum97@gmail.com>
2015-03-24 23:24:20 -0400

    added MyStack

---
+++ 09Stacks/MyStack.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class MyStack<T>{
+
+    private LinkedList stack;
+
+    public MyStack(){
+	stack = new LinkedList();
+    }
+
+    public String name(){
+	return "majumder,abhimannu";
+    }
+
+    public T push(T val){
+	stack.add(0,val);
+	return val;
+    }
+
+    public T pop(){
+	return stack.remove(0);
+    }
+
+    public boolean isEmpty(){
+	return stack.size() == 0;
+    }
+}
