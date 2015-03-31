715a7d1f52ea5b5f09de3b2a53f2f912731fefd9
Abhimannu Majumder <abhimannum97@gmail.com>
2015-03-31 13:21:08 -0400

    started Maze and added Coordinate

---
+++ 11BFS/Coordinate.java
@@ -0,0 +1,20 @@
+public class Coordinate{
+    int x, y;
+    
+    public int getX(){
+	return x;
+    }
+
+    public int getY(){
+	return y;
+    }
+
+    public void setX(int u){
+	x = u;
+    }
+
+    public void setY(int u){
+	y = u;
+    }
+
+}
