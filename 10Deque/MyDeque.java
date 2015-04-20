a7d2869614925218a0e64ad65843452bd6636c1b
Abhimannu Majumder <abhimannum97@gmail.com>
2015-04-19 21:38:42 -0400

    almost done

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -43,59 +43,83 @@ public class MyDeque<T>{
 	deq = temp;
     }
 
-    public void add(T value){
-	if (isEmpty() || tail == deq.length - 1){
+    public void add(int ind, T value){
+	if (isEmpty()){ //tail == deq.length - 1){
 	    deq[0] = value;
 	    tail = 0;
+	} else if (ind < 0){
+	    deq[deq.length - 1] = value;
 	}else{
-	    tail++;
-	    deq[tail] = value;
+	    deq[ind] = value;
 	}
-    } 
+    }
 	
 	
     
     public void addFirst(T value){
-	copy(1);
-	if (head != 0){
-	    head = head - 1;
+	if (head == 0){
+	    head = deq.length - 1;
 	}else{
-	    head = size - 1;
+	    head -= 1;
 	}
+	capacity++;
 	deq[head] = value;
+	//resize();
     }
+	    
 	
-	
-    /*
     public void addLast(T value){
-
+	tail++;
+	capacity++;
+	add(tail,value);
+	//resize();
     }
-
+    
     public T removeFirst() throws NoSuchElementException{
-
+	T value = (T)deq[head];
+	if (isEmpty()){
+	    throw new NoSuchElementException();
+	} else if (capacity == 1){
+	    add(head, null);
+	    head = 0;
+	    tail = 0;
+	    capacity--;
+	}
+	add(head,null);
+	head++;
+	capacity--;
+	//	resize();
+	return value;
     }
 
     public T removeLast() throws NoSuchElementException{
-
+	T value = (T)deq[tail];
+	if (isEmpty()){
+	    throw new NoSuchElementException();
+	} else {
+	    deq[tail] = null;
+	    tail--;
+	    capacity--;
+	    //resize();
+	    return value;
+	}
     }
     
+    public boolean wrap(){
+	if (deq[deq.length - 1] != null && deq[0] != null){
+	    return true;
+	}else 
+	    return false;
+    }
 
     public Object[] resize(){
-	Object[] ary = new Object[size*2];
-	int ind = 0;
-	while (head < size){
-	    ary[ind] = deq[head];
-	    ind++;
-	    head++;
-	}
-	if (deq[0] != null){
-	    int i = 0;
-	    while (i < tail+1)
-	    
-	}
-    }
-    */
-
+	if (capacity > size/2){
+	    Object[] ary = new Object[size*2];
+	    if (wrap()){
+		for (int i = 0; i < tail+1; i++){
+		    
+    
+    
     public String toString(){
 	String str = "[ ";
 	for (int i = 0; i < deq.length; i++){
@@ -113,7 +137,7 @@ public class MyDeque<T>{
     
     public static void main(String [] args){
 	
-	MyDeque<T> test = new MyDeque();
+	MyDeque test = new MyDeque();
 	
 	test.add(0);
 	test.add(1);
