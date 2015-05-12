3953bf8536d6b76c846f8ec328c45876e6fc88b9
Abhimannu Majumder <abhimannum97@gmail.com>
2015-05-12 13:14:41 -0400

    added a bunch of methods and currently working on add

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,18 +1,21 @@
+import java.util.*;
 
 public class MyHeap{
 
     int[] heap;
-    int root;
+    int root, index, length;
     
     public MyHeap(){
 	heap = new int[10];
-	root = 1;
+	root = 1;			      
+	index = 1;
+	length = 0;
     }
     
     public boolean isEmpty(){
 	int i = 0;
 	while ( i < heap.length){
-	    if (heap[i] != null){
+	    if (heap[i] != 0){
 		return false;
 	    }
 	    i++;
@@ -21,19 +24,63 @@ public class MyHeap{
     }
 
     public boolean isEmpty(int ind){
-	if (heap[ind] != null){
+	if (heap[ind] != 0){
 	    return false;
 	}
 	return true;
     }
 
-    public void add(int k){
-	if (isEmpty()){
-	    heap[root] = k;
+    public int getRight(){
+	if (isEmpty(index)){
+	    return -1;
 	}
-	int i = 0;
-	while (!isEmpty(i)){
-	    i++;
+	index = 2 * index;
+	return index;
+    }
+    
+    public int getLeft(){
+	if (isEmpty(index)){
+	    return -1;
 	}
-	heap[i] = k;
+	index = 2 * index + 1;
+	return index;
+    }
+
+    public int getParent(int ind){
+	return ind / 2;
+    }
+    
+    public String toString(){
+	String str = "";
+	for (int i = 0; i <= length; i++){
+	    str += heap[i];
+	}
+	return str;
+    }
+
+    public peek(){
+	return heap[1];
+    }
 	
+    public void add(int k){
+	length++;
+	if (isEmpty()){
+	    heap[index] = k;
+	} else {
+	    heap[index] = k;
+	}
+	pushUp(index);
+	index++;
+    }
+    
+    public void pushUp(int ind){
+	int temp = ind;
+	while (getParent(temp) < heap[ind]){
+	    temp = getParent(temp);
+	    
+
+    public static void main(String[]args){
+	
+	MyHeap heap = new MyHeap();
+
+}
