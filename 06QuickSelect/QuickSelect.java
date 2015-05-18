cf2d8bd97298f5227cf32004bc2ce2e23574736f
Abhimannu Majumder <abhimannum97@gmail.com>
2015-05-17 20:26:37 -0400

    *

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -10,61 +10,67 @@ public class QuickSelect{
 	return "[" + ans + "]";
     }
 
+    /*
     public static void oldpartition(int[] ary, int si, int ei){
 	System.out.println(Arrays.toString(ary));
 	int[] D = new int[ary.length];
-	for (int i = 0; i < ary.length; i++){
+	int i = 0;
+	while ( i < ary.length){  
 	    if (i < si || i > ei){
 		D[i] = ary[i];
 	    }
 	}
-	int start = si;
-	int end = ei;
-	int ri = si + (int)(Math.random() * (ei - si + 1));
-	int pivot = ary[ri];
+	int s = si;
+	int e = ei;
+	Random r = new Random();
+	int ri = si + r.nextInt(ei-si+1);
+	int p = ary[ri];
 	System.out.println("pivot =" + pivot);
 	System.out.println(Arrays.toString(D));
-	for (int i = start; i <= end; i++){
-	    if(ary[i] < pivot){
+	i = si;
+	while (i <= e){
+	    if(ary[i] < p){
 		D[si] = ary[i];
 		si++;
-	    }else if (ary[i] > pivot){
+	    }else if (ary[i] > p){
 		D[ei] = ary[i];
 		ei--;
 	    }
-	    
-	     System.out.println(Arrays.toString(D));
+	    i++;
+	    System.out.println(Arrays.toString(D));
 	}
-	D[ri] = pivot;
+	D[ri] = p;
 	System.out.println(Arrays.toString(D));
     }
+    */
 
     public static int partition(int[] ary, int si, int ei){
 	System.out.println(Arrays.toString(ary));
-	int ri = ary[si + (int)(Math.random() * (ei - si))];
+	Random r = new Random();
+	int ri = ary[si + r.nextInt(ei-si)];
 	System.out.println("pivot = " + ri);
-	int trace = si;
+	int t = si;
 	int holder;
-	while (si != ei){
-	    if (ary[trace] > ri){
+	do {
+	    if (ary[t] > ri){
 		holder = ary[ei];
-		ary[ei] = ary[trace];
+		ary[ei] = ary[t];
 		ary[si] = holder;
 		ei--;
 	    }
-	    if (ary[trace] < ri){
+	    if (ary[t] < ri){
 		si++;
-		trace++;
+		t++;
 	    }
-	    if (ary[trace] == ri){
-		trace++;
+	    if (ary[t] == ri){
+		t++;
 	    }
 	    System.out.println(Arrays.toString(ary));
-	}
-	//ary[si] = ri;
+	} while (si != ei);
+	ary[si] = ri;
 	return si;
     }
-
+    
     public static int quickSelect(int ary[], int n){
 	return 4;
     }
